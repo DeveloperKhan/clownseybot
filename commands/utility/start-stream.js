98c3d6130d6884156c605c753fc940523b6fd2c6
DeveloperKhan <adib.khan@gmail.com>
2024-12-06 18:00:24 -0800

    update start-stream

--- commands/utility/start-stream.js
+++ commands/utility/start-stream.js
@@ -8,13 +8,23 @@ module.exports = {
 		.setName('start-stream')
 		.setDescription('Start stream on Dracoviz homepage')
 		.addStringOption(option =>
+			option.setName('channel')
+				.setDescription('Twitch channel name (do not include twitch.tv)')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('title')
+				.setDescription('Title on the homepage')
+				.setRequired(true))
+		.addStringOption(option =>
 			option.setName('link')
-				.setDescription('https://www.youtube.com/embed/...')
+				.setDescription('Link to dracoviz page')
 				.setRequired(true)),
 	async execute(interaction) {
-		const currentStream = interaction.options.getString('link');
-		if (currentStream == null || !currentStream.includes('https://www.youtube.com/embed')) {
-			await interaction.reply('Invalid link. Must be a YouTube embed link.');
+		const currentStream = interaction.options.getString('channel');
+		const title = interaction.options.getString('title');
+		const link = interaction.options.getString('link');
+		if (currentStream == null || currentStream.includes('twitch.tv')) {
+			await interaction.reply('Invalid link. Must be a Twitch channel name.');
 			return;
 		}
 		const client = new MongoClient(uri);
@@ -22,6 +32,8 @@ module.exports = {
 		try {
 			const updated = await pokemongo.collection('stream').updateOne({ }, { $set:{
 				currentStream,
+				title,
+				link,
 				active: true,
 			} });
 			if (updated == null) {
